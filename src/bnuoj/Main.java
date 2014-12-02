03c361068ff5dac70480e771a708a005486e5049
Hu Zhe <huzhe@travelsky.com>
2014-12-02 15:20:11 +0800

    wrong answer

---
+++ src/bnuoj/Main.java
@@ -0,0 +1,59 @@
+package bnuoj;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()){
+			String line = sc.nextLine();
+
+			Scanner lsc = new Scanner(line);
+			String s1 = lsc.next();
+			String s2 = lsc.next();
+			
+			if (s1.equals("0")&&s2.equals("0")){
+//				System.out.println("ooo");
+				break;
+			}
+			
+			int minLength = s1.length()<s2.length()?s1.length():s2.length();
+
+
+			char[] s_s1 = String.valueOf(s1).toCharArray();
+			char[] s_s2 = String.valueOf(s2).toCharArray();
+			
+			int result = 0;
+			int go = 0;
+			for (int i = 0;i<minLength;i++){
+
+				;
+				int i_s1 = Integer.valueOf(String.valueOf(s_s1[s1.length() -1 -i]));
+				int i_s2 = Integer.valueOf(String.valueOf(s_s2[s2.length() -1 -i]));
+				if ((i_s1 + i_s2 + go)>=10){
+					result = result + 1;
+					go = 1;
+				}else{
+					go =0;
+				}
+			}
+			
+			if (result ==0){
+				System.out.println("No carry operation.");
+			}else{
+				if (result ==1){
+					System.out.println("1 carry operation.");
+				}else{
+					System.out.println(result  + " carry operations.");
+				}
+			}
+			
+			
+			
+		}
+	}
+
+}
